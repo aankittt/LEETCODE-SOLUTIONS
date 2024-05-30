f60590a889b729823c0a59847aba1f8b98e4e9b8
ANKIT TRIPATHI <117687642+aankittt@users.noreply.github.com>
2024-05-30 18:04:51 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (67.04%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1442-count-triplets-that-can-form-two-arrays-of-equal-xor/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int count=0;
+        for(int i=0;i<arr.size();i++)
+        {   
+            int ans=0;
+            for(int j=i;j<arr.size();j++)
+            {
+                ans=ans^arr[j];
+                if((j-i)>=1)
+                {
+                    if(ans==0)
+                    count+=j-i;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
